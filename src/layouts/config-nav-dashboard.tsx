bc7d06380db2c171d7a84b2f682af530b6b4de95
Bethel Fiagbetor <110622280+litoguy@users.noreply.github.com>
2024-10-08 23:40:45 +0000

    first commit

---
+++ src/layouts/config-nav-dashboard.tsx
@@ -0,0 +1,46 @@
+import { Label } from 'src/components/label';
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => (
+  <SvgColor width="100%" height="100%" src={`/assets/icons/navbar/${name}.svg`} />
+);
+
+export const navData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: icon('ic-analytics'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  {
+    title: 'Gallery',
+    path: '/gallery',
+    icon: icon('ic-cart'),
+    info: (
+      <Label color="error" variant="inverted">
+        *
+      </Label>
+    ),
+  },
+  {
+    title: 'Posts',
+    path: '/posts',
+    icon: icon('ic-blog'),
+  },
+  {
+    title: 'Sign in',
+    path: '/sign-in',
+    icon: icon('ic-lock'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: icon('ic-disabled'),
+  },
+];
